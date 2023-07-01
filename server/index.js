9a43e55c0fedc3823075986a0692123453b5151b
Veda <vedacr2510@gmail.com>
2023-07-02 00:05:46 +0530

    Updated login

--- server/index.js
+++ server/index.js
@@ -18,55 +18,8 @@ mongoose.connect(url, (err) => {
 app.use(cors());
 app.use(express.json());
 app.use("/mentee",menteeroute)
-  
-app.post("/login",async (req, res) => {
-  await Mentee.findOne({username : req.body.username})
-    .then((user) => {
 
-      if(req.body.password===user.password)
-      {
-        return res.status(200).json({
-                  message: "Login Successful",
-                  email: user.email,
-                  role: "Mentee"
-                });
-      }
-      else
-      {
-        return res.status(400).json({
-                  message: "Passwords does not match",
-                  error,
-                }); 
-      }
-    })
-    await Mentor.findOne({username : req.body.username})
-    .then((user) => {
 
-      if(req.body.password===user.password)
-      {
-        return res.status(200).json({
-                  message: "Login Successful",
-                  email: user.email,
-                  role: "Mentor"
-                });
-      }
-      else
-      {
-        return res.status(400).json({
-                  message: "Passwords does not match",
-                  error,
-                }); 
-      }
-    })
-    // catch error if email does not exis
-    .catch((error) => {
-     return res.status(400).json({
-       message: "User doesnot exist",
-       error,
-     });
-     console.error(error);
-   });
-});
 
 
 
